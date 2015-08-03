bd7f657ab11a138d6e0d5355502509df20180c23
Neal Robben <neal.robben@gmail.com>
2015-08-03 21:12:39 +0200

    test

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testlist = new List<int> { 1, 2, 3 };
+            var testlist2 = new List<int> { 1, 2, 3, 4, 5 };
+
+            foreach (int nValue in testlist)
+            {
+                var result = testlist2.Where(x => x == nValue).Any();
+            }
+
+        }
+    }
+}
